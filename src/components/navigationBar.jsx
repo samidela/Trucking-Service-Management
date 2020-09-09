d98d49255ed3aab5a0b5635d9981f6757d1200ea
Samuel Eyob <saeyob78@gmail.com>
2020-09-08 19:16:52 -0500

    made changes to index file

--- src/components/navigationBar.jsx
+++ src/components/navigationBar.jsx
@@ -1,28 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar, NavDropdown, Form } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, Form, NavbarBrand } from "react-bootstrap";
 
 class NavigationBar extends Component {
   render() {
     return (
       <div>
         <div className="header container-fluid">
-          <div className=" header-title bg-info text-white text-center p-2 w3-black w3-center w3-allerta">
-            <h4 className="w3-xxxlarge">Gary Trucking LLC</h4>
+          <div className=" header-title bg-info text-white text-center  w3-black w3-center w3-allerta">
+            <NavbarBrand className="w3-xxxlarge">
+              <Link to="/home "> Gary Trucking LLC</Link>
+            </NavbarBrand>
           </div>
         </div>
         <Navbar bg="light" expand="lg" className="justify-content-between">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto ">
-              <NavDropdown title="Services" id="basic-nav-dropdown" className="pl-5">
+              <NavDropdown
+                title="Services"
+                id="basic-nav-dropdown"
+                className="pl-5"
+              >
                 <NavDropdown.Item href="/Services/biennial">
                   Biennial
                 </NavDropdown.Item>
                 <NavDropdown.Item href="/Services/cabcard">
                   Cab Card
                 </NavDropdown.Item>
-                <NavDropdown.Item href="/Services/carrierPacket" >
+                <NavDropdown.Item href="/Services/carrierPacket">
                   Carrier Packet
                 </NavDropdown.Item>
                 <NavDropdown.Item href="/Services/dispatch">
@@ -36,7 +42,11 @@ class NavigationBar extends Component {
                   Ucr Registration
                 </NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown title="Current Driver" id="basic-nav-dropdown" className="pl-5">
+              <NavDropdown
+                title="Current Driver"
+                id="basic-nav-dropdown"
+                className="pl-5"
+              >
                 <NavDropdown.Item href="/CurrentDrivers/bol">
                   BOL Uploader
                 </NavDropdown.Item>
@@ -44,7 +54,11 @@ class NavigationBar extends Component {
                   Login
                 </NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown title="Future Driver" id="basic-nav-dropdown" className="pl-5">
+              <NavDropdown
+                title="Future Driver"
+                id="basic-nav-dropdown"
+                className="pl-5"
+              >
                 <NavDropdown.Item href="/FutureDrivers/EligibilityChecker">
                   Eligibility
                 </NavDropdown.Item>
