bdd3cac9c9450295ee3c57be81e1d9009b4980bd
Samuel Eyob <saeyob78@gmail.com>
2020-09-09 23:57:34 -0500

    layout changes to carrier packet

--- src/components/Services/carrierPacket.jsx
+++ src/components/Services/carrierPacket.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const CarrierPacket = () => (
-  <div className="layout">
+  <div className="layout jumbotron jumbotron-fluid">
     <h2>Work with us </h2>
     <p>You have questions call us</p>
   </div>
