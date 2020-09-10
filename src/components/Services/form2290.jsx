c52ff7176efbe9827bf853c68c02ed987f5d9e37
Samuel Eyob <saeyob78@gmail.com>
2020-09-09 23:58:14 -0500

    layout changes to form 2290

--- src/components/Services/form2290.jsx
+++ src/components/Services/form2290.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Form2290 = () => (
-  <div className="layout">
+  <div className="layout jumbotron jumbotron-fluid">
     <h2>Work with us </h2>
     <p>You have questions call us</p>
   </div>
