54e7d2c7191d34195973a2f775ddd9b6302849a3
Samuel Eyob <saeyob78@gmail.com>
2020-09-10 23:53:14 -0500

    the info in biennial file has changed

--- src/components/Services/biennial.jsx
+++ src/components/Services/biennial.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import Form from "../common/form";
+import Joi from "joi-browser";
 
-export const Biennial = () => (
-  <div className="layout jumbotron jumbotron-fluid">
-    <h2>Work with us </h2>
-    <p>You have questions call us</p>
-  </div>
-);
+class Biennial extends Form {
+  state = {
+    data: {
+      name: "",
+      phone: "",
+      email: "",
+      dot: "",
+      description: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    name: Joi.string().required().label("Name"),
+    phone: Joi.number().required().label("Phone"),
+    email: Joi.string().email().required().label("Email"),
+    dot: Joi.number().required().label("DOT"),
+    description: Joi.string(),
+  };
+
+  doSubmit = () => {
+    console.log("form submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Biennial </h1>
+        <p>
+          The biennial update is a requirement set by the FMCSA to update your
+          MCS-150 Form every two years. Most states require an update as part of
+          renewing your truck/fleet registration.
+        </p>
+        <form className=" layout ">
+          {this.renderInput("name", "Name")}
+          {this.renderInput("phone", "Phone")}
+          {this.renderInput("email", "Email")}
+          {this.renderInput("dot", "DOT")}
+          {this.renderInput("description", "Description")}
+          {this.renderButton("Submit")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Biennial;
