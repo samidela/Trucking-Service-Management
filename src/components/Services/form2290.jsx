fd4c7c1b2657abae908d774192738ffbe2748059
Samuel Eyob <saeyob78@gmail.com>
2020-09-10 23:51:34 -0500

    made some changes to the description of form 2290

--- src/components/Services/form2290.jsx
+++ src/components/Services/form2290.jsx
@@ -1,8 +1,55 @@
 import React from "react";
+import Form from "../common/form";
+import Joi from "joi-browser";
 
-export const Form2290 = () => (
-  <div className="layout jumbotron jumbotron-fluid">
-    <h2>Work with us </h2>
-    <p>You have questions call us</p>
-  </div>
-);
+class form2290 extends Form {
+  state = {
+    data: {
+      name: "",
+      phone: "",
+      email: "",
+      dot: "",
+      description: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    name: Joi.string().required().label("Name"),
+    phone: Joi.number().required().label("Phone"),
+    email: Joi.string().email().required().label("Email"),
+    dot: Joi.number().required().label("DOT"),
+    description: Joi.string(),
+  };
+
+  doSubmit = () => {
+    console.log("form submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>IRP Cab Card </h1>
+        <p>
+          If you plan on carrying cargo through more than one state, you will
+          need to get an International Registration Plan (IRP). This allows you
+          to haul loads through the contiguous 48 states and Canada with a
+          single registration. You will receive an apportioned license plate.
+          The registration fee is usually $1,500-$2,000, and varies by state and
+          vehicle weight. Here is the state list with links to each
+          jurisdiction.
+        </p>
+        <form className=" layout ">
+          {this.renderInput("name", "Name")}
+          {this.renderInput("phone", "Phone")}
+          {this.renderInput("email", "Email")}
+          {this.renderInput("dot", "DOT")}
+          {this.renderInput("description", "Description")}
+          {this.renderButton("Submit")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default form2290;
