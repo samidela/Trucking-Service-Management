894d8514ecd8633612c13b7cf80632e5c193832b
Samuel Eyob <saeyob78@gmail.com>
2020-09-10 23:52:20 -0500

    made some changes to the description of carrier packet info

--- src/components/Services/carrierPacket.jsx
+++ src/components/Services/carrierPacket.jsx
@@ -1,8 +1,49 @@
 import React from "react";
+import Form from "../common/form";
+import Joi from "joi-browser";
 
-export const CarrierPacket = () => (
-  <div className="layout jumbotron jumbotron-fluid">
-    <h2>Work with us </h2>
-    <p>You have questions call us</p>
-  </div>
-);
+class CarrierPacket extends Form {
+  state = {
+    data: {
+      name: "",
+      phone: "",
+      email: "",
+      dot: "",
+      description: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    name: Joi.string().required().label("Name"),
+    phone: Joi.number().required().label("Phone"),
+    email: Joi.string().email().required().label("Email"),
+    dot: Joi.number().required().label("DOT"),
+    description: Joi.string(),
+  };
+
+  doSubmit = () => {
+    console.log("form submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Carrier Packet </h1>
+        <p>
+          
+        </p>
+        <form className=" layout ">
+          {this.renderInput("name", "Name")}
+          {this.renderInput("phone", "Phone")}
+          {this.renderInput("email", "Email")}
+          {this.renderInput("dot", "DOT")}
+          {this.renderInput("description", "Description")}
+          {this.renderButton("Submit")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CarrierPacket;
