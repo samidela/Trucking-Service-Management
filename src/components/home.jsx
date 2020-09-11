fb1edcca52868c6d1eb514963035a04e750fa81e
Samuel Eyob <saeyob78@gmail.com>
2020-09-10 23:54:00 -0500

    email was added to the homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 export const Home = () => (
   <div className="layout jumbotron jumbotron-fluid">
-    <h2>Work with us </h2>
-    <p>You have questions call us</p>
+    <h1 className="home-h1 font-color: blue">Need Sevice? </h1>
+    <br />
+    <h2 className="home-h2">Click on one of the tabs to get started</h2>
+    <p className="home-p3">
+      Our goal at Gary Trucking LLC is to provide our customers with the highest
+      quality of service and safety in the trucking industry. Excellence in
+      service and safety are company goals. We measure ourselves by the high
+      standards of our customers, and value their input. Mutual progress towards
+      defined goals is our objective in working with our customers.
+    </p>
   </div>
 );
