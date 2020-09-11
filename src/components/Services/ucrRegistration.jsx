ec62c655e230b3da7e18f74d5a1d12bf2be21d50
Samuel Eyob <saeyob78@gmail.com>
2020-09-10 23:51:02 -0500

    made some changes to the description of ucr

--- src/components/Services/ucrRegistration.jsx
+++ src/components/Services/ucrRegistration.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import Form from "../common/form";
+import Joi from "joi-browser";
 
-export const UcrRegistration = () => (
-  <div>
-    <h2 className="layout jumbotron jumbotron-fluid">Work with us </h2>
-    <p>You have questions call us</p>
-  </div>
-);
+class UcrRegistration extends Form {
+  state = {
+    data: {
+      name: "",
+      phone: "",
+      email: "",
+      dot: "",
+      description: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    name: Joi.string().required().label("Name"),
+    phone: Joi.number().required().label("Phone"),
+    email: Joi.string().email().required().label("Email"),
+    dot: Joi.number().required().label("DOT"),
+    description: Joi.string(),
+  };
+
+  doSubmit = () => {
+    console.log("form submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Ifta </h1>
+        <p>
+          The Unified Carrier Registration (UCR) is a federal system of
+          registering and collecting fees from operators of vehicles
+          participating in interstate commerce.
+        </p>
+        <form className=" layout ">
+          {this.renderInput("name", "Name")}
+          {this.renderInput("phone", "Phone")}
+          {this.renderInput("email", "Email")}
+          {this.renderInput("dot", "DOT")}
+          {this.renderInput("description", "Description")}
+          {this.renderButton("Submit")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UcrRegistration;
