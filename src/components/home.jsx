18fba6f520337506a0d4a57b345b48f2399409ab
Samuel Eyob <saeyob78@gmail.com>
2020-09-11 23:59:53 -0500

    made some

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import road from "../Assets/road.jpg";
+import { Jumbotron, Button } from "react-bootstrap";
 
 export const Home = () => (
-  <div className=" jumbotron jumbotron-fluid  ">
-    <h1 className="home-h1">Need Sevice? </h1>
-    <br />
-    <h2 className="home-h2">Click on one of the tabs to get started</h2>
-    <p className="home-p3">
-      Our goal at Gary Trucking LLC is to provide our customers with the highest
-      quality of service and safety in the trucking industry. Excellence in
-      service and safety are company goals. We measure ourselves by the high
-      standards of our customers, and value their input. Mutual progress towards
-      defined goals is our objective in working with our customers.
-    </p>
+  <div className="layout jumbotron jumbotron-fluid container ">
+    <Jumbotron>
+      <h1>Welcome!</h1>
+      <p>
+        Our goal at Gary Trucking LLC is to provide our customers with the
+        highest quality of service and safety in the trucking industry.
+        Excellence in service and safety are company goals. We measure ourselves
+        by the high standards of our customers, and value their input. Mutual
+        progress towards defined goals is our objective in working with our
+        customers.
+      </p>
+      <p>
+        <Button variant="primary">Learn more</Button>
+      </p>
+    </Jumbotron>
   </div>
 );
+
+export default Home;
