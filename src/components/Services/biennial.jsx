bf22ebc32db65fa7dabe13fe0f54f3cbd3ffa905
Samuel Eyob <saeyob78@gmail.com>
2020-09-12 03:23:08 -0500

    changed the form a bit in biennial

--- src/components/Services/biennial.jsx
+++ src/components/Services/biennial.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "../common/form";
 import Joi from "joi-browser";
+import { Container, Col, Row } from 'react-bootstrap';
 
 class Biennial extends Form {
   state = {
@@ -28,21 +29,27 @@ class Biennial extends Form {
 
   render() {
     return (
-      <div className="row jumbotron jumbotron-fluid pl-4">
-        <h1>Biennial </h1>
-        <p>
-          The biennial update is a requirement set by the FMCSA to update your
-          MCS-150 Form every two years. Most states require an update as part of
-          renewing your truck/fleet registration.
-        </p>
-        <form className=" layout ">
-          {this.renderInput("name", "Name")}
-          {this.renderInput("phone", "Phone")}
-          {this.renderInput("email", "Email")}
-          {this.renderInput("dot", "DOT")}
-          {this.renderInput("description", "Description")}
-          {this.renderButton("Submit")}
-        </form>
+      <div className="row jumbotron jumbotron-fluid pl-4 pr-3 layout">
+        <Container>
+          <Row>
+            <Col className=" form-group mx-sm-3 mb-2 ">
+              <h1>Biennial </h1>
+              <p>
+                The biennial update is a requirement set by the FMCSA to update
+                your MCS-150 Form every two years. Most states require an update
+                as part of renewing your truck/fleet registration.
+              </p>
+              <form >
+                {this.renderInput("name", "Name")}
+                {this.renderInput("phone", "Phone")}
+                {this.renderInput("email", "Email")}
+                {this.renderInput("dot", "DOT")}
+                {this.renderInput("description", "Description")}
+                {this.renderButton("Submit")}
+              </form>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
