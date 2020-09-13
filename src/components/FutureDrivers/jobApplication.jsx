002426585792a030c2683b4355b301173a4446d4
Samuel Eyob <saeyob78@gmail.com>
2020-09-13 00:57:14 -0500

    updated the job application

--- src/components/FutureDrivers/jobApplication.jsx
+++ src/components/FutureDrivers/jobApplication.jsx
@@ -29,18 +29,37 @@ class JobApplication extends Form {
   render() {
     return (
       <div className="row jumbotron jumbotron-fluid pl-4">
-        <h1>Biennial </h1>
+        <h1>Job Application </h1>
         <p>
-          Apply Today 
+          Gary Trucking LLC is constantly looking for talented individuals that
+          can deliver the goods our customers need. If you are looking for an
+          exciting opportunity with a company that's clearly going places. Apply
+          today to see if you are eligible
         </p>
-        <form className=" layout ">
+        <form className=" column container">
           {this.renderInput("name", "Name")}
           {this.renderInput("phone", "Phone")}
           {this.renderInput("email", "Email")}
-          {this.renderInput("dot", "DOT")}
           {this.renderInput("description", "Description")}
+          {
+            <div class="form-group">
+              {" "}
+              <label for="exampleInputFile">Upload Driver License </label>{" "}
+              <input type="file" id="exampleInputFile"></input>
+              <p class="help-block"></p>{" "}
+            </div>
+          }
           {this.renderButton("Submit")}
         </form>
+        <div style={{color:"red"}}className="column container pl-5">
+          <h3>Required Materials</h3>
+          <ul>
+            <li>21 years old</li>
+            <li>Class A CDL</li>
+            <li>1 year of experience (Preferred)</li>
+            <li> Must Have a Clean Record</li>
+          </ul>
+        </div>
       </div>
     );
   }
