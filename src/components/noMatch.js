e51255029cec5c221276561a660acec874ad2570
Samuel Eyob <saeyob78@gmail.com>
2020-09-13 00:56:56 -0500

    made no match a contacts page

--- src/components/noMatch.js
+++ src/components/noMatch.js
@@ -2,6 +2,24 @@ import React from "react";
 
 export const NoMatch = () => (
   <div>
-    <h2>No Match</h2>
+    <h1 style={{ color: "#cdce "}} >Contact US</h1>
+    <ul style={{ color: "#fdfd ", fontSize: "18px" }} className="container pl-4 pt-3 ">
+      Call - (469) 602-15722
+      <br />
+      Email - Garytruckingllc@yahoo.com
+      <br />
+      <br />
+      <br />
+      Our office is located at:
+      <br />
+      <p
+        style={{ color: "#ffdf", fontSize: "22px" }}
+        className="float-center pl-4"
+      >
+        9304 Forest Ln S141
+        <br />
+        Garland, TX 75231
+      </p>
+    </ul>
   </div>
 );
