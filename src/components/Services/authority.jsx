1ae5d67418dd9ccf2ac3473b4e0569a306da9609
Samuel Eyob <saeyob78@gmail.com>
2020-09-14 10:40:26 -0500

    made some styling changes

--- src/components/Services/authority.jsx
+++ src/components/Services/authority.jsx
@@ -18,8 +18,6 @@ class Authority extends Form {
     name: Joi.string().required().label("Name"),
     phone: Joi.number().required().label("Phone"),
     email: Joi.string().email().required().label("Email"),
-    dot: Joi.number().required().label("DOT"),
-    description: Joi.string(),
   };
 
   doSubmit = () => {
@@ -28,9 +26,9 @@ class Authority extends Form {
 
   render() {
     return (
-      <div className="row jumbotron jumbotron-fluid pl-4">
+      <div className="row jumbotron jumbotron-fluid pl-4 pr-3">
         <h1 className="widget-title">Get Your Authority </h1>
-        <p>
+        <p className="container">
           So we heard you’re thinking about going fully independent. You’ve
           probably decided to get your own authority so you can cut out the
           middleman and keep more income for yourself while operating more
@@ -38,31 +36,18 @@ class Authority extends Form {
           government’s permission to get paid for hauling freight as your own
           trucking company.
         </p>
-        <form className="column container">
+        <form className="col-xs-4 container column">
           {this.renderInput("name", "Name")}
           {this.renderInput("phone", "Phone")}
           {this.renderInput("email", "Email")}
           {this.renderButton("Submit")}
         </form>
         <div style={{ color: "red" }} className="column container pl-5">
-          {/* <h3>Required Materials</h3>
-          <ul>
-            <li>
-              Run under your own MC number - we’ll take care of all the
-              paperwork
-            </li>
-            <li>
-              We’ll file your federal and state permits, so you don’t have to
-              worry about what step is next
-            </li>
-            <li>
-              Get guidance through the whole process, even after you’re up and
-              running
-            </li>
-            <li>
-              Avoid delays by letting the experts take care of the legal stuff
-            </li>
-          </ul> */}
+          <ul style={{ color: "red" }} className="pr-3 float-right  ">
+            <h3>Required Materials</h3>
+            <li>MC</li>
+            <li>Insured Truck and Trailer</li>
+          </ul>
         </div>
       </div>
     );
